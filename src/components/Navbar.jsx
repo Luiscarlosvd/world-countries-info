b1937a27626ba7d8f53b3929b096b589be9d318c
Luiscarlosvd <lc26cr7@gmail.com>
2023-06-22 14:26:47 -0400

    Add icons, style and fonts for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
-import { NavLink } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { IoMdSettings, IoIosArrowBack } from 'react-icons/io';
+import { BsFillMicFill } from 'react-icons/bs';
 
 const Navbar = () => (
-  <header className="flex justify-between px-16 items-center py-4 mb-8 border border-b-2 bg-blue-800">
-    <div className="flex items-center gap-5">
-      <h1 className="text-2xl text-white">Football Standings</h1>
+  <header className="flex justify-between px-6 items-center py-3 mb-8 border border-b-2 bg-blue-800">
+    <div>
+      <Link to="/"><IoIosArrowBack className="text-xl text-white mr-5" /></Link>
     </div>
-    <ul className="flex gap-5 text-blue-500">
-      <li>
-        <NavLink to="/">
-          Rockets
-        </NavLink>
-      </li>
-    </ul>
+    <h1 className="text-xl text-white text-lato-300">Leagues</h1>
+    <div className="flex gap-4">
+      <IoMdSettings className="text-xl text-white" />
+      <BsFillMicFill className="text-xl text-white" />
+    </div>
   </header>
 );
 
